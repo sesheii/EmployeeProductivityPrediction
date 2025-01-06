7d455ec076987811e7d977100a37c749743eb7c5
Your Name <you@example.com>
2025-01-06 19:33:10 +0200

    fix UI

--- epp/prediction/views.py
+++ epp/prediction/views.py
@@ -3,12 +3,26 @@ from django import forms
 import pandas as pd
 import os
 from datetime import date
+import hashlib
+import json
+from sklearn.model_selection import KFold
+import joblib
+import warnings
+
+warnings.filterwarnings("ignore", category=UserWarning)
+
+def hash_map_with_md5(data: dict) -> str:
+    json_data = json.dumps(data, sort_keys=True)
+    md5_hash = hashlib.md5()
+    md5_hash.update(json_data.encode('utf-8'))
+    return md5_hash.hexdigest()
 
 
 def unique_df(df, column):
     return sorted([i for i in df[f"{column}"].unique()])
 
 original_dataset = pd.read_csv(os.path.join("data/HRDataset_v14.csv"))
+pd.set_option('display.max_columns', None)  
 
 
 def calculate_age(dob, reference_date):
@@ -18,9 +32,36 @@ def calculate_age(dob, reference_date):
     if dob > reference_date:
         raise ValueError("Date of Birth cannot be after the reference date")
 
-    # Calculate age based on the reference date
     age = reference_date.year - dob.year - ((reference_date.month, reference_date.day) < (dob.month, dob.day))
     return age
+
+
+def kfold_target_encoding_weighted_mean(df, categorical_cols, target_col, n_splits=7, m=10):
+    df_encoded = df.copy()
+    global_mean = df[target_col].mean()
+
+    kf = KFold(n_splits=n_splits, shuffle=True, random_state=42)
+
+    for col in categorical_cols:
+        df_encoded[f'{col}_encoded'] = 0.0
+        for train_idx, val_idx in kf.split(df):
+            train_data, val_data = df.iloc[train_idx], df.iloc[val_idx]
+
+            category_means = train_data.groupby(col, observed=False)[target_col].mean()
+            category_counts = train_data.groupby(col, observed=False)[target_col].count()
+
+            smoothed_means = (category_means * category_counts + global_mean * m) / (category_counts + m)
+
+            test = val_data[col].map(smoothed_means)
+            has_nan = test.isnull().any()
+            if has_nan:
+                val_encoded = val_data[col].map(smoothed_means).fillna(global_mean)
+            else:
+                val_encoded = val_data[col].map(smoothed_means)
+
+            df_encoded.loc[val_idx, f'{col}_encoded'] = val_encoded.astype(float)
+
+    return df_encoded
 
 
 def process_form_data(form_data):
@@ -44,26 +85,91 @@ def process_form_data(form_data):
         results["Age"] = "Both DOB and LastPerformanceReview_Date are required for age calculation"
 
     for field_name, value in form_data.items():
-        if field_name == "Married":
+        if field_name == "MarriedID":
             if value == "Yes":
                 results["MarriedID"] = 1
             else:
                 results["MarriedID"] = 0
 
-        if field_name in ["Salary", "EmpSatisfaction", "SpecialProjectsCount", "DaysLateLast30", "Absences"]:
+        if field_name in ["Salary", "EngagementSurvey", "EmpSatisfaction", "SpecialProjectsCount", "DaysLateLast30", "Absences"]:
             results[field_name] = int(value)
 
-        if field_name == "Salary":
-            results["Salary"] = int(value)
+        if field_name in ["Position", "State", "Sex", "RaceDesc", "ManagerName", "Department", "RecruitmentSource", "MaritalDesc", "CitizenDesc"]:
+            results[field_name] = value
+        
+        # elif field_name not in ["DOB", "LastPerformanceReview_Date"]:
+        #     results[field_name] = f"{value}"
 
-        if field_name == "Salary":
-            results["Salary"] = int(value)
+    hash_hex = hash_map_with_md5(results)
+    hash_decimal = int(hash_hex, 16)
+    results["EmpID"] = hash_decimal
+    # results["PerformanceScore"] = "Exceeds"
+    results["PerformanceScore"] = "Fully Meets"
+    
 
+    selected_columns = ["EmpID", "DOB", "LastPerformanceReview_Date", "MarriedID", "Salary", "EngagementSurvey", "EmpSatisfaction", 
+                        "SpecialProjectsCount", "DaysLateLast30", "Absences", "Position", "State", "Sex", "RaceDesc", 
+                        "ManagerName", "Department", "RecruitmentSource", "MaritalDesc", "CitizenDesc", "PerformanceScore"]
+    df = original_dataset[selected_columns].copy()
 
-        elif field_name not in ["DOB", "LastPerformanceReview_Date"]:
-            results[field_name] = f"{value}"
+    df["DOB"] = pd.to_datetime(df["DOB"], errors="coerce")
+    df["LastPerformanceReview_Date"] = pd.to_datetime(df["LastPerformanceReview_Date"], errors="raise", format='%m/%d/%Y')
+    df['Age'] = (df['LastPerformanceReview_Date'] - df['DOB']).dt.days // 365
+    df = df.drop(columns=[
+        "DOB", "LastPerformanceReview_Date"
+    ])
 
-    return results
+    new_record_df = pd.DataFrame([results])
+    df = pd.concat([df, new_record_df], ignore_index=True)
+    df['EmpID'] = pd.to_numeric(df['EmpID'], errors='coerce')
+    df['Age'] = pd.to_numeric(df['Age'], errors='coerce')
+
+    col_cat = list(df.select_dtypes(include=['object']).columns)
+    for i in col_cat:
+        if i != 'PerformanceScore':
+            df[i] = df[i].astype('category')
+    col_cat = list(df.select_dtypes(include=['category']).columns)
+    
+    performance_score_mapping = {
+        "PIP": 0,
+        "Needs Improvement": 1,
+        "Fully Meets": 2,
+        "Exceeds": 3
+    }
+
+    df['PerformanceScore'] = df['PerformanceScore'].map(performance_score_mapping)
+    df['PerformanceScore'] = pd.to_numeric(df['PerformanceScore'], errors='coerce')
+
+    df_encoded = kfold_target_encoding_weighted_mean(df, col_cat, target_col='PerformanceScore', n_splits=5, m=10)
+    df_encoded = df_encoded.drop(columns=col_cat)
+
+    encoded_record = df_encoded[df_encoded["EmpID"] == hash_decimal].iloc[0]
+    encoded_record = pd.DataFrame([encoded_record.drop(["EmpID", "PerformanceScore"])])
+    
+
+    clf = joblib.load("dump/RandomForestClassifier.pkl")
+    expected_features = clf.feature_names_in_
+    # print("Expected features:", expected_features)
+
+    encoded_record = encoded_record[clf.feature_names_in_]
+    results_array = clf.predict(encoded_record)
+    results_scalar = results_array[0]
+
+    reverse_performance_score_mapping = {
+        0: "PIP",
+        1: "Needs Improvement",
+        2: "Fully Meets",
+        3: "Exceeds",
+    }
+    # print(encoded_record.columns)
+    # print(encoded_record.info())
+    # print(df[df["EmpID"] == hash_decimal])
+    # print(df_encoded[df_encoded["EmpID"] == hash_decimal])
+    # print(df.info())
+    # print(df_encoded.info())
+    # print(df.head())
+    # print(str(results))
+    return reverse_performance_score_mapping.get(results_scalar)
 
 
 def dynamic_form_view(request):
@@ -85,7 +191,7 @@ def dynamic_form_view(request):
             "desc": "Дата останнього проведення оцінки продуктивності.",
         },
         {
-            "name": "Married",
+            "name": "MarriedID",
             "type": "category",
             "label": "MarriedID",
             "required": True,
@@ -148,7 +254,7 @@ def dynamic_form_view(request):
             "desc": "Загальна кількість відсутностей працівника.",
         },
         {
-            "name": "Position_encoded", # encode
+            "name": "Position", # _encoded
             "type": "category",
             "label": "Position",
             "required": True,
@@ -156,7 +262,7 @@ def dynamic_form_view(request):
             "desc": "Посада працівника.",
         },
         {
-            "name": "State_encoded", # encode
+            "name": "State", # _encoded
             "type": "category",
             "label": "State",
             "required": True,
@@ -164,7 +270,7 @@ def dynamic_form_view(request):
             "desc": "Штат, де живе людина.",
         },
         {
-            "name": "Sex_encoded", # encode
+            "name": "Sex", # _encoded
             "type": "category",
             "label": "Sex",
             "required": True,
@@ -172,7 +278,7 @@ def dynamic_form_view(request):
             "desc": "Стать працівника.",
         },
         {
-            "name": "RaceDesc_encoded", # encode
+            "name": "RaceDesc", # _encoded
             "type": "category",
             "label": "RaceDesc",
             "required": True,
@@ -180,15 +286,15 @@ def dynamic_form_view(request):
             "desc": "опис раси, до якої людина себе відносить.",
         },
         {
-            "name": "ManagerName_encoded", # encode
+            "name": "ManagerName", # _encoded
             "type": "category",
             "label": "ManagerName",
             "required": True,
-            "choices": unique_df(original_dataset, "RaceDesc"),
+            "choices": unique_df(original_dataset, "ManagerName"),
             "desc": "Ім'я менеджера, який відповідає за працівника.",
         },
         {
-            "name": "Department_encoded", # encode
+            "name": "Department", # _encoded
             "type": "category",
             "label": "Department",
             "required": True,
@@ -196,7 +302,7 @@ def dynamic_form_view(request):
             "desc": "Відділ, у якому працює працівник.",
         },
         {
-            "name": "RecruitmentSource_encoded", # encode
+            "name": "RecruitmentSource", # _encoded
             "type": "category",
             "label": "RecruitmentSource",
             "required": True,
@@ -204,7 +310,7 @@ def dynamic_form_view(request):
             "desc": "Джерело, через яке працівник був найнятий.",
         },
         {
-            "name": "MaritalDesc_encoded", # encode
+            "name": "MaritalDesc", # _encoded
             "type": "category",
             "label": "MaritalDesc",
             "required": True,
@@ -212,7 +318,7 @@ def dynamic_form_view(request):
             "desc": "Сімейний стан працівника.",
         },
         {
-            "name": "CitizenDesc_encoded", # encode
+            "name": "CitizenDesc", # _encoded
             "type": "category",
             "label": "CitizenDesc",
             "required": True,
@@ -278,7 +384,8 @@ def dynamic_form_view(request):
             if form.is_valid():
                 form_data = {field: form.cleaned_data[field] for field in form.cleaned_data}
                 results = process_form_data(form_data)
-                response_message = " ".join(str(results.values()))
+                # response_message = " ".join(str(results))
+                response_message = str(results)
             else:
                 response_message = "There were errors in the form. Please correct them."
     else:
